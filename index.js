fd013fd2b4c7f51bfb8c4cada7104a2cc630e0ef
MarvNC <rotrobmin@gmail.com>
2021-11-29 15:48:01 -0800

    command handlers tutorial

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-// Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
-// Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-// When the client is ready, run this code (only once)
 client.once('ready', () => {
-	console.log('Ready!');
+  console.log('Ready!');
+  setInterval(() => {
+    client.user.setActivity(
+      `${client.guilds.cache.size} servers | ${client.users.cache.size} users`,
+      { type: 'WATCHING' }
+    );
+  }, 60000);
 });
 
-// Login to Discord with your client's token
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === 'ping') {
+    await interaction.reply('Pong!');
+  } else if (commandName === 'server') {
+    await interaction.reply(
+      `Server name: ${interaction.guild.name}
+Total members: ${interaction.guild.memberCount}
+Creation date: ${interaction.guild.createdAt}
+Total channels: ${interaction.guild.channels.cache.size}`
+    );
+  } else if (commandName === 'user') {
+    await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+  }
+});
+
 client.login(token);
