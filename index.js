75c9e07f544b0b3d4a515e29b9f0d885745ac796
MarvNC <rotrobmin@gmail.com>
2021-11-29 16:04:43 -0800

    follow guide

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
-const { Client, Intents } = require('discord.js');
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  // Set a new item in the Collection
+  // With the key as the command name and the value as the exported module
+  client.commands.set(command.data.name, command);
+}
+
+function repeat() {
+  client.user.setActivity(`${client.guilds.cache.size} servers`, { type: 'WATCHING' });
+}
+
 client.once('ready', () => {
   console.log('Ready!');
-  setInterval(() => {
-    client.user.setActivity(
-      `${client.guilds.cache.size} servers | ${client.users.cache.size} users`,
-      { type: 'WATCHING' }
-    );
-  }, 60000);
+  repeat();
+  setInterval(repeat, 60000);
 });
 
 client.on('interactionCreate', async (interaction) => {
   if (!interaction.isCommand()) return;
 
-  const { commandName } = interaction;
+  const command = client.commands.get(interaction.commandName);
 
-  if (commandName === 'ping') {
-    await interaction.reply('Pong!');
-  } else if (commandName === 'server') {
-    await interaction.reply(
-      `Server name: ${interaction.guild.name}
-Total members: ${interaction.guild.memberCount}
-Creation date: ${interaction.guild.createdAt}
-Total channels: ${interaction.guild.channels.cache.size}`
-    );
-  } else if (commandName === 'user') {
-    await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: 'There was an error while executing this command!',
+      ephemeral: true,
+    });
   }
 });
 
